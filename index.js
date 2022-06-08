c65e648c3ca09f0530f5853d4fdce4ea44cf5e03
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-06-08 08:52:49 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    return customerName.toUpperCase();
+    //return customerName.toUpperCase();
+}
+function setBestCustomer () {
+    return bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer () {
+   return bestCustomer = "maybe bob"
+}
+
+const leastFavoriteCustomer = "Kim";
+
+function changeLeastFavoriteCustomer () {
+    return leastFavoriteCustomer = "Sam"
+}
+
+upperCaseCustomerName()
